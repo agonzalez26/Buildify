477306f5d854736114759a5dcdb293584aecfb5e
agonzalez26 <agonzalez26@student.gsu.edu>
2017-10-31 22:57:06 -0400

    Update

--- src/buildify/Controller/Template1Controller.java
+++ src/buildify/Controller/Template1Controller.java
@@ -6,12 +6,11 @@ import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.scene.control.Alert;
 import buildify.Buildify;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import java.io.File;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -33,13 +32,12 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
-import javax.imageio.ImageIO;
 
 public class Template1Controller implements Initializable {
     // variables
 
     ObservableList<String> comboItemsList = FXCollections.observableArrayList(
-            "Label", "Button", "TextField", "TextArea");
+            "Label", "Button", "TextField", "TextArea", "Image");
     @FXML
     private ScrollPane template1View;
     @FXML
@@ -78,7 +76,12 @@ public class Template1Controller implements Initializable {
     @FXML
     private Button imageChooser;
     private ImageView  myImageView;
-
+    private FileChooser chooser;
+    private File fr;
+    private Alert a;
+    private Scene scene;
+    private Image img;
+    
     @FXML
     private void handleButtonAction(ActionEvent event) throws IOException {
         // checks which button does what
@@ -107,7 +110,7 @@ public class Template1Controller implements Initializable {
         if (event.getSource() == back) {
             stage = (Stage) menuBar.getScene().getWindow();
             root = FXMLLoader.load(Buildify.class.getResource("View/TemplateView.fxml"));
-            Scene scene = new Scene(root);
+            scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
         } else if (event.getSource() == openT) {
@@ -119,7 +122,7 @@ public class Template1Controller implements Initializable {
         } else if (event.getSource() == saveExit) {
             System.out.println("Saving and exit.");
         } else if (event.getSource() == help) {
-            Alert a = new Alert(Alert.AlertType.ERROR, "First Name Empty", ButtonType.OK);
+            a = new Alert(Alert.AlertType.ERROR, "First Name Empty", ButtonType.OK);
             Optional<ButtonType> result = a.showAndWait();
         }else {
 
@@ -131,31 +134,77 @@ public class Template1Controller implements Initializable {
     }
 
     @FXML
+    private void handleComboBoxAction() throws IOException{
+//        comboList.getSelectionModel().getSelectedItem();
+//        System.out.println(comboList.getValue());
+//        if(comboList.getValue() == "label"){
+////            NameText.
+//            NameText.setVisible(true);
+//        }
+
+//        NameText.
+        comboList.getSelectionModel().selectedItemProperty().addListener(
+                (ob, old_val, new_val) -> {
+                    if(comboList.getValue() == "Button"){
+//                        System.out.println("button");
+                        NameText.setVisible(true);
+                    }else{
+//                        System.out.println("aasds");
+                        NameText.setVisible(false);
+                    }
+                }
+        );
+    }
+    @FXML
     private void handleImageChooser(ActionEvent event) throws IOException {
         if(event.getSource() == imageChooser){
-            FileChooser chooser = new FileChooser();
+            chooser = new FileChooser();
             chooser.setTitle("Choose a file");
-            File fr = chooser.showOpenDialog(stage); 
-            chooser.getExtensionFilters().addAll(new ExtensionFilter("Image Files", "jpg", "png", "gif"));
+            chooser.getExtensionFilters().addAll(new ExtensionFilter("Image Files", "*.jpg", "*.png", "*.gif"));
+            fr = chooser.showOpenDialog(stage); 
+
+//             Image img =  new Image(Buildify.class.getResourceAsStream("apple.jpg"));
+//            img = new Image(fr.toURI().toString());
+//            myImageView.setImage(img);
             if(fr != null){//if nothing is selected, then it is null and will not print anything
                 imageChooser.setText(fr.getName());
-//                BufferedImage bufferedImage = ImageIO.read(fr);
-//                WritableImage image = SwingFXUtils.toFXImage(bufferedImage, null);
-//                myImageView.setImage(image);
-//               Image img = new Image("https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png?201703170823");
-//        ImageView iview = new ImageView(img);
-//        ImageView iview = new ImageView(new Image("htt"));
-//                designPane.add(myImageView, 2,2 );
-                
+                String imagePath = fr.toURI().toURL().toString();
+                System.out.println(fr.toURI().toString());
+                img = new Image(imagePath);
+                myImageView = new ImageView(img);
+                designPane.add(myImageView, 2,2 );
+//                
             }else{
                 System.out.println("Nothing was selected");
             }
+
+//            FileChooser chooser = new FileChooser();
+//                chooser.setTitle("Choose a file");
+//                chooser.showOpenDialog(stage);
+//                //record the file
+//                File f = chooser.showOpenDialog(stage); 
+////              File f = chooser.showSaveDialog(getOwnerWindow());// will warn to save the file to cancel or replace, returns a path and you can do anything to file
+//                
+//                if(f != null){//if nothing is selected, then it is null and will not print anything
+//                    System.out.println(f.getName()); //prints name of file
+//                                    imageChooser.setText(f.getName());
+//
+//                }else{
+//                       System.out.println("Nothing was selected");
+//                }
         }
     }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // setting the values into the choice box
         comboList.setItems(comboItemsList);
+//        NameText.setVisible(false);
+//         comboList.getSelectionModel().getSelectedItem();
+//        System.out.println(comboList.getValue());
+//        if(comboList.getValue() == "label"){
+////            NameText.
+//            NameText.setVisible(true);
+//        }
     }
 
    
