4d2ed73a0c1bcc09f61b635a7aece41392c99b31
Maia Ross <rainbowroach@gmail.com>
2017-11-12 12:11:03 -0500

    starting to plan out the model, have some open questions and probably need to talk to alma about it!!!

---
+++ src/buildify/Model/SaveableWidget.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buildify.Model;
+
+import javafx.scene.Node;
+
+/**
+ *
+ * @author Maia
+ */
+public class SaveableWidget {
+    Node node;
+    
+    public SaveableWidget(Node node){
+        this.node = node;
+    }
+}
---
+++ src/buildify/Model/Writer.java
@@ -0,0 +1,27 @@
+/*
+    Works cited for my solutions so far:
+
+    http://www.mkyong.com/java/jaxb-hello-world-example/
+    https://stackoverflow.com/questions/16240734/how-to-marshal-unmarshal-java-objects-with-private-fields-using-jaxb
+ */
+package buildify.Model;
+
+import java.io.File;
+
+/**
+ *
+ * @author Maia
+ */
+public class Writer {
+    File target;
+    
+    
+    public Writer(File f){
+        target = f;
+    }
+    
+    public void addLabel(){
+        
+    }
+    
+}
